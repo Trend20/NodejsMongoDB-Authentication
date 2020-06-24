57da920c661f02a040dbc23a810b5c7db3ab38c2
Trend20 <enockomondi305@gmail.com>
2020-06-24 16:06:16 -0400

    email

--- app/middleware/verifySignup.js
+++ app/middleware/verifySignup.js
@@ -16,3 +16,22 @@ checkDuplicateUsernameOrEmail = (req, res, next) => {
       res.status(400).send({ message: "Failed! Username is already in use!" });
       return;
     }
+
+    // Email
+    User.findOne({
+        email: req.body.email
+      }).exec((err, user) => {
+        if (err) {
+          res.status(500).send({ message: err });
+          return;
+        }
+  
+        if (user) {
+          res.status(400).send({ message: "Failed! Email is already in use!" });
+          return;
+        }
+  
+        next();
+      });
+    });
+  };
